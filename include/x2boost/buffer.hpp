005002ecc937b2b089985d88d17bc7cc35af614f
Jae-jun Kang <jaykang920@gmail.com>
2015-04-26 20:55:20 +0900

    ported x2::buffer class

--- include/x2boost/buffer.hpp
+++ include/x2boost/buffer.hpp
@@ -10,6 +10,11 @@
 
 #include <deque>
 
+namespace boost { namespace asio {
+    class const_buffer;
+    class mutable_buffer;
+} }
+
 namespace x2
 {
     // A variable-length byte buffer class whose capacity is limited to a
@@ -38,30 +43,69 @@ namespace x2
         // Sets the current zero-based position.
         void pos(int value);
 
-        //void list_available_buffers(std::vector<boost::asio::mutable_buffer>& result)
-        //{
-            
-        //}
-
-        void read_fixed(boost::int32_t& value)
+        void list_occupied_buffers(std::vector<boost::asio::const_buffer>& result) const
         {
-            check_length_to_read(4);
-            value = read_byte();
-            value = (value << 8) | read_byte();
-            value = (value << 8) | read_byte();
-            value = (value << 8) | read_byte();
+            list_const_buffers(result, front_, back_);
+        }
+        void list_starting_buffers(std::vector<boost::asio::const_buffer>& result, int length) const
+        {
+            list_const_buffers(result, front_, front_ + length);
+        }
+        void list_ending_buffers(std::vector<boost::asio::const_buffer>& result, int length) const
+        {
+            list_const_buffers(result, back_ - length, back_);
+        }
+        // Retrieves the occupied const buffers with the specified range.
+        void list_const_buffers(std::vector<boost::asio::const_buffer>& result, int begin, int end) const;
+        // Retrieves the available mutable buffers.
+        void list_mutable_buffers(std::vector<boost::asio::mutable_buffer>& result);
+
+        void mark_to_read(int length)
+        {
+            if ((front_ + length) > back_)
+            {
+                // exception
+                return;
+            }
+            marker_ = front_ + length;
         }
 
-        void write_fixed(boost::int32_t& value)
+        // Reads a sequence of bytes from the buffer and advances the position.
+        void read(byte_t* buffer, int offset, int count);
+
+        // Alias of pos(0).
+        void rewind() { pos(0); }
+
+        void shrink(int length);
+        void stretch(int length);
+
+        byte_t* to_array() const;
+
+        void trim();
+
+        // Writes a sequence of bytes to the buffer and advances the position.
+        void write(byte_t* buffer, int offset, int count);
+
+        byte_t& operator[] (int index)
         {
-            ensure_capacity_to_write(4);
-            write_byte((byte_t)(value >> 24));
-            write_byte((byte_t)(value >> 16));
-            write_byte((byte_t)(value >> 8));
-            write_byte((byte_t)value);
+            index += front_;
+            return blocks_[index >> block_size_exponent_][index & remainder_mask_];
+        }
+
+        const byte_t& operator[] (int index) const
+        {
+            index += front_;
+            return blocks_[index >> block_size_exponent_][index & remainder_mask_];
         }
 
     private:
+        // Copies a sequence of bytes from the specified byte array segment
+        // to the buffer.
+        void copy_from(int pos, byte_t* buffer, int offset, int count);
+        // Copies a sequence of bytes from the buffer
+        // to the specified byte array segment.
+        void copy_to(int pos, byte_t* buffer, int offset, int count) const;
+
         void block_feed()
         {
             if ((pos_ & remainder_mask_) == 0 && (pos_ & ~remainder_mask_) != 0)
@@ -82,13 +126,13 @@ namespace x2
         void cleanup();
         void ensure_capacity_to_write(int n);
 
-        byte_t read_byte()
+        byte_t get_byte()
         {
             block_feed();
             return block_[pos_++ & remainder_mask_];
         }
 
-        void write_byte(byte_t value)
+        void put_byte(byte_t value)
         {
             block_feed();
             block_[pos_++ & remainder_mask_] = value;
--- src/buffer.cpp
+++ src/buffer.cpp
@@ -3,6 +3,11 @@
 
 #include "x2boost/buffer.hpp"
 
+#include <algorithm>
+#include <cstring>
+
+#include <boost/asio/buffer.hpp>
+
 #include "x2boost/util/buffer_pool.hpp"
 
 using namespace x2;
@@ -39,6 +44,36 @@ void buffer::cleanup()
     block_ = 0;
 }
 
+void buffer::copy_from(int pos, byte_t* buffer, int offset, int count)
+{
+    int block_index = pos >> block_size_exponent_;
+    int dst_offset = pos & remainder_mask_;
+    int bytes_to_copy, bytes_copied = 0;
+    while (bytes_copied < count)
+    {
+        bytes_to_copy = std::min(block_size() - dst_offset, count - bytes_copied);
+        ::memcpy(blocks_[block_index++] + dst_offset,
+            buffer + offset + bytes_copied, bytes_to_copy);
+        dst_offset = 0;
+        bytes_copied += bytes_to_copy;
+    }
+}
+
+void buffer::copy_to(int pos, byte_t* buffer, int offset, int count) const
+{
+    int block_index = pos >> block_size_exponent_;
+    int src_offset = pos & remainder_mask_;
+    int bytes_to_copy, bytes_copied = 0;
+    while (bytes_copied < count)
+    {
+        bytes_to_copy = std::min(block_size() - src_offset, count - bytes_copied);
+        ::memcpy(buffer + offset + bytes_copied,
+            blocks_[block_index++] + src_offset, bytes_to_copy);
+        src_offset = 0;
+        bytes_copied += bytes_to_copy;
+    }
+}
+
 void buffer::ensure_capacity_to_write(int n)
 {
     int required = pos_ + n;
@@ -49,6 +84,43 @@ void buffer::ensure_capacity_to_write(int n)
     if (required > back_)
     {
         back_ = required;
+    }
+}
+
+void buffer::list_const_buffers(std::vector<boost::asio::const_buffer>& result, int begin, int end) const
+{
+    int begin_index = begin >> block_size_exponent_;
+    int begin_offset = begin & remainder_mask_;
+    int end_index = end >> block_size_exponent_;
+    int end_offset = end & remainder_mask_;
+    if (begin_index == end_index)
+    {
+        result.push_back(boost::asio::buffer(blocks_[begin_index] + begin_offset, end_offset - begin_offset));
+        return;
+    }
+    result.push_back(boost::asio::buffer(blocks_[begin_index] + begin_offset, block_size() - begin_offset));
+    for (int i = begin_index + 1; i < end_index; ++i)
+    {
+        result.push_back(boost::asio::buffer(blocks_[i], block_size()));
+    }
+    if (end_offset != 0)
+    {
+        result.push_back(boost::asio::buffer(blocks_[end_index], end_offset));
+    }
+}
+
+void buffer::list_mutable_buffers(std::vector<boost::asio::mutable_buffer>& result)
+{
+    if ((capacity() - back_) < block_size())
+    {
+        blocks_.push_back(buffer_pool::acquire(block_size_exponent_));
+    }
+    int back_index = back_ >> block_size_exponent_;
+    int back_offset = back_ & remainder_mask_;
+    result.push_back(boost::asio::buffer(blocks_[back_index] + back_offset, block_size() - back_offset));
+    for (int i = back_index + 1; i < (int)blocks_.size(); ++i)
+    {
+        result.push_back(boost::asio::buffer(blocks_[i], block_size()));
     }
 }
 
@@ -68,6 +140,91 @@ void buffer::pos(int value)
         block_index_ = block_index;
         block_ = blocks_[block_index_];
     }
+}
+
+void buffer::read(byte_t* buffer, int offset, int count)
+{
+    check_length_to_read(count);
+    copy_to(pos_, buffer, offset, count);
+    pos(pos() + count);
+}
+
+void buffer::shrink(int length)
+{
+    if ((front_ + length) > back_)
+    {
+        // exception
+        return;
+    }
+    front_ += length;
+    if (pos_ < front_)
+    {
+        pos(0);
+    }
+}
+
+void buffer::stretch(int length)
+{
+    if ((back_ + length) > capacity())
+    {
+        // exception
+        return;
+    }
+    back_ += length;
+}
+
+byte_t* buffer::to_array() const
+{
+    byte_t* array = new byte_t[length()];
+    copy_to(front_, array, 0, length());
+    return array;
+}
+
+void buffer::trim()
+{
+    int index, count;
+    if (marker_ >= 0)
+    {
+        if (pos_ < marker_)
+        {
+            pos(marker_ - front_);
+        }
+        marker_ = -1;
+    }
+    if (pos_ == back_)
+    {
+        index = 1;
+        count = (int)blocks_.size() - 1;
+        front_ = back_ = 0;
+    }
+    else
+    {
+        index = 0;
+        count = pos_ >> block_size_exponent_;
+        if (count >= (int)blocks_.size())
+        {
+            count = (int)blocks_.size() - 1;
+        }
+        back_ -= block_size() * count;
+        front_ = pos_ & remainder_mask_;
+    }
+    if (count > 0)
+    {
+        int limit = index + count;
+        for (int i = index; i < limit; ++i)
+        {
+            buffer_pool::release(block_size_exponent_, blocks_[i]);
+        }
+        blocks_.erase(blocks_.begin() + index, blocks_.begin() + limit);
+    }
+    pos(0);
+}
+
+void buffer::write(byte_t* buffer, int offset, int count)
+{
+    ensure_capacity_to_write(count);
+    copy_from(pos_, buffer, offset, count);
+    pos(pos() + count);
 }
 
 // EOF buffer.cpp

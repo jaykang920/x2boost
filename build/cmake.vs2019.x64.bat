2e9dd1730f32199d40cd9fa5fa0007a737e2f92c
Jae-jun Kang <jaykang920@gmail.com>
2020-09-13 13:21:22 +0900

    Update cmake.vs2019.x64.bat

--- build/cmake.vs2019.x64.bat
+++ build/cmake.vs2019.x64.bat
@@ -9,6 +9,6 @@ if exist "%OUTDIR%" (
 mkdir "%OUTDIR%"
 
 pushd "%OUTDIR%"
-cmake -G "Visual Studio 16 2019" -A Win64 -D BUILD_TEST=ON -D BUILD_SAMPLES=ON ..\..
+cmake -G "Visual Studio 16 2019" -D BUILD_TEST=ON -D BUILD_SAMPLES=ON ..\..
 popd
 endlocal

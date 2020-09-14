80f4b93bd5cde0b4e9b72d86656065b3a95843a4
Jae-jun Kang <jaykang920@gmail.com>
2020-09-14 18:21:06 +0900

    Update string.hpp

--- include/x2boost/util/string.hpp
+++ include/x2boost/util/string.hpp
@@ -8,9 +8,7 @@
 #include "x2boost/pre.hpp"
 #endif
 
-//#include <iostream>
-
-namespace x2boost
+namespace
 {
     template<class T>
     std::ostream& operator<<(std::ostream& out, const boost::intrusive_ptr<T>& ptr)
@@ -20,32 +18,6 @@ namespace x2boost
         return out;
     }
 
-    template<class T>
-    std::istream& operator>>(std::istream& in, boost::intrusive_ptr<T>& ptr)
-    {
-        //cout << "Enter Real Part ";
-        //in >> c.real;
-        //cout << "Enter Imaginary Part ";
-        //in >> c.imag;
-        return in;
-    }
-
-    std::ostream& operator<<(std::ostream& out, const std::vector<std::string>& list)
-    {
-        //out << c.real;
-        //out << "+i" << c.imag << endl;
-        return out;
-    }
-
-    std::istream& operator>>(std::istream& in, std::vector<std::string>& list)
-    {
-        //cout << "Enter Real Part ";
-        //in >> c.real;
-        //cout << "Enter Imaginary Part ";
-        //in >> c.imag;
-        return in;
-    }
-
     template<typename T>
     std::ostream& operator<<(std::ostream& out, const std::vector<T>& list)
     {
@@ -53,48 +25,6 @@ namespace x2boost
         //out << "+i" << c.imag << endl;
         return out;
     }
-
-    template<typename T>
-    std::istream& operator>>(std::istream& in, std::vector<T>& list)
-    {
-        //cout << "Enter Real Part ";
-        //in >> c.real;
-        //cout << "Enter Imaginary Part ";
-        //in >> c.imag;
-        return in;
-    }
-
-    /*
-    template<class T>
-    std::ostream& operator<<(std::ostream& out, std::vector<boost::intrusive_ptr<T>>& list)
-    {
-        //out << c.real;
-        //out << "+i" << c.imag << endl;
-        return out;
-    }
-
-    template<class T>
-    std::istream& operator>>(std::istream& in, std::vector<boost::intrusive_ptr<T>>& list)
-    {
-        //cout << "Enter Real Part ";
-        //in >> c.real;
-        //cout << "Enter Imaginary Part ";
-        //in >> c.imag;
-        return in;
-    }
-
-    template<class T>
-    std::string to_string(boost::intrusive_ptr<T>& ptr)
-    {
-        return "";
-    }
-
-    template<typename T>
-    std::string to_string(std::vector<boost::intrusive_ptr<T>>& list)
-    {
-        return "";
-    }
-    */
 }
 
 #endif  // X2BOOST_STRING_HPP_
